4501aa91d741e6037752cd14b5a2d774063c5ce8
Gui <chigui2@cc-login1.campuscluster.illinois.edu>
2024-10-27 21:11:52 -0500

    Offline Models For Type A data

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 from OpenAI_Chat import GPT4O
+from Client import Client
 from pydantic import BaseModel
 import json
 import multiprocessing
@@ -7,10 +8,12 @@ from tqdm import tqdm
 import argparse
 
 class Generate:
-    def __init__(self, raw_data_file, output_file, model_name="gpt-4o", num_processes=None):
+    def __init__(self, raw_data_file, output_file, model_name="gpt-4o", openai_api_base="", num_processes=None):
         self.raw_data_file = raw_data_file
         self.output_file = output_file
-        self.model_name = model_name
+        self.offline_model = model_name
+        self.model_name = model_name.split("/")[-1]
+        self.openai_api_base = openai_api_base
         # If the number of processes is not specified, use the number of CPU cores
         self.num_processes = num_processes if num_processes is not None else os.cpu_count()
 
@@ -27,7 +30,10 @@ class Generate:
     def process_item(self, args):
         item, model_name, output_file, lock = args
         prompt = self.get_prompt(item)
-        client = GPT4O(model_name=model_name, messages=[])
+        if self.model_name == "gpt-4o" or self.model_name == "gpt-4o-mini":
+            client = GPT4O(model_name=model_name, messages=[])
+        else:
+            client = Client(model_name=self.offline_model, openai_api_base=self.openai_api_base, messages=[])
         
         try:
             response = client.chat(prompt=prompt["user"], images=prompt["images"])
@@ -105,8 +111,9 @@ if __name__ == "__main__":
     parser.add_argument("--input_file", type=str, required=True, help="Path to the input JSON file.")
     parser.add_argument("--output_file", type=str, required=True, help="Path to the output JSONL file.")
     parser.add_argument("--model_name", type=str, default="gpt-4o", help="Model name to use.")
+    parser.add_argument("--openai_api_base", type=str, default="", help="Base URL for OpenAI API.")
     parser.add_argument("--num_processes", type=int, default=os.cpu_count(), help="Number of processes to use.")
     args = parser.parse_args()
 
-    reformatter = Generate(raw_data_file=args.input_file, output_file=args.output_file, model_name=args.model_name, num_processes=args.num_processes)
+    reformatter = Generate(raw_data_file=args.input_file, output_file=args.output_file, model_name=args.model_name, num_processes=args.num_processes, openai_api_base=args.openai_api_base)
     reformatter.generate()
